86e7a926aaf48cb2881e95c1ad49806d38f89d5e
alexey-mr <alexey.morlang@gmail.com>
2020-02-25 13:07:32 +0300

    Fixed download images script
    - provisioner should be downloaded conditionally for builds >2001
    
    closes-jira-bug: CEM-12909
    Change-Id: I43dd4a5325c33058826f731845916f4961552758

--- tools/contrail/import_contrail_container.sh
+++ tools/contrail/import_contrail_container.sh
@@ -133,7 +133,6 @@ DockerContrailKafkaImageName:contrail-external-kafka
 DockerContrailNeutronPluginImageName:contrail-openstack-neutron-init
 DockerContrailNodeInitImageName:contrail-node-init
 DockerContrailNodemgrImageName:contrail-nodemgr
-DockerContrailProvisionerImageName:contrail-provisioner
 DockerContrailNovaPluginImageName:contrail-openstack-compute-init
 DockerContrailRabbitmqImageName:contrail-external-rabbitmq
 DockerContrailRedisImageName:contrail-external-redis
