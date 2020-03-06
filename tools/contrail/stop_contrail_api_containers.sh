5aa9d953a3bcf322643f3399a3f6e43dbc4f89ac
gleb108@gmail.com <gleb108@gmail.com>
2020-03-06 17:13:34 +0300

    ZIU fix: stopping containers
    partial-jira-bug: CEM-13336
    Change-Id: Ib201bfa790de2cf6a318d3caff1f3c74da5f7bc9

--- tools/contrail/stop_contrail_api_containers.sh
+++ tools/contrail/stop_contrail_api_containers.sh
@@ -4,5 +4,13 @@
 #list containers that must be stopped before update procedure
 STOP_CONTAINERS=${STOP_CONTAINERS:-'contrail_config_api contrail_analytics_api'}
 
-echo Stopping contrail config services:
-sudo docker stop $STOP_CONTAINERS
+
+#Detecting containers to stop
+for container in $STOP_CONTAINERS; do
+    check=$(docker ps --format '{{.Names}}' | grep -c "$container")
+    if [ $check -gt 0 ]; then
+       echo Found container $container. Stopping
+       sudo docker stop $container
+    fi
+done
+
